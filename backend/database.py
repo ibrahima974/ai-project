b61c4465a9e49573ca97718bcd0dd3386c1c0faa
ibrahima974 <ibrahimangaidengaide@gmail.com>
2026-03-21 19:09:41 +0000

    feat: setup FastAPI app with SQLAlchemy and CORS

---
+++ backend/database.py
@@ -0,0 +1,26 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
+
+load_dotenv(encoding="utf-8")
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL n'est pas définie dans le fichier .env")
+
+print("DEBUG URL:", DATABASE_URL)
+
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
