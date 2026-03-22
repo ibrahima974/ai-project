322d949b74ddd32b005450539234cd663b4cefc2
ibrahima974 <ibrahimangaidengaide@gmail.com>
2026-03-22 18:10:11 +0000

    fix url vercel

--- backend/main.py
+++ backend/main.py
@@ -12,7 +12,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://insightiq.vercel.app"
+        "https://ai-project-steel-psi.vercel.app/"
     ],
     allow_credentials=True,
     allow_methods=["*"],
